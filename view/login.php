431dc435abc0b887e12f48f199bc2dd15d52bbb1
[Caio <[caiosilvasantos87@gmail.com]>
2025-05-14 23:07:20 -0300

    Segurança HASH PASSWORD

--- view/login.php
+++ view/login.php
@@ -4,43 +4,43 @@ include('../model/conexao.php');
 
 ## Verificação da existencia de dados email e senha
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
 
-        if (empty($_POST['email'])){
-            echo "Preencha seu email";
-            exit;
+    if (empty($_POST['email'])){
+        echo "Preencha seu email";
+        exit;
 
-        } else if(empty($_POST['senha'])) {  
-            echo "Preencha sua senha";
-            exit;
-        }
+    } else if(empty($_POST['senha'])) {  
+        echo "Preencha sua senha";
+        exit;
+    }
 
-        #evitar sqlinjection -> limpa a string
+    #evitar sqlinjection -> limpa a string
+    $email = $mysqli->real_escape_string($_POST['email']);
+    $senha = $_POST['senha']; // Não aplicar real_escape_string em senha para usar password_verify corretamente
 
-        $email = $mysqli->real_escape_string($_POST ['email']);
-        $senha = $mysqli->real_escape_string($_POST ['senha']);
+    $sql_code = "SELECT * FROM usuarios WHERE email = '$email'";
+    $sql_result = $mysqli->query($sql_code);
 
-        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
-        $sql_result = $mysqli->query($sql_code);
+    if ($sql_result->num_rows == 1) {
+        $usuario = $sql_result->fetch_assoc();
 
-        if ($sql_result->num_rows == 1) {
-            #Login bem sucedido
-                $usuario = $sql_result->fetch_assoc();
-                $_SESSION['email'] = $email;
-                $_SESSION['id'] = $usuario['id'];
-                $_SESSION['tipo_usuario'] = $usuario['tipo_usuario'];
+        if (password_verify($senha, $usuario['senha'])) {
+            $_SESSION['email'] = $email;
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['tipo_usuario'] = $usuario['tipo_usuario'];
 
-                #Separando redirecionamento de medicos e usuarios
-                if ($usuario['tipo_usuario'] == 1) {
-                    header("Location: tabelamedicoinfo.html");
-                }else {
-                    header("Location: painel.php");
-                }        
-        }else {
+            if ($usuario['tipo_usuario'] == 1) {
+                header("Location: tabelamedicoinfo.html");
+            } else {
+                header("Location: painel.php");
+            }
+        } else {
             echo "Usuario ou Senha incorretos";
         }
+    } else {
+        echo "Usuario ou Senha incorretos";
+    }
 };
 
 $mysqli->close();
-
 ?>
